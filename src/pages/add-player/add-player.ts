0ff65c5d9cf1b3155d3ff6b1f62925d1691a97f5
Rui Júnio Silva <ruca.33.maia@gmail.com>
2018-11-20 18:35:08 +0000

    added add-player form

--- src/pages/add-player/add-player.ts
+++ src/pages/add-player/add-player.ts
@@ -2,17 +2,28 @@ import { Component } from '@angular/core';
 
 import { ViewController } from 'ionic-angular';
 
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'page-add-player',
   templateUrl: 'add-player.html'
 })
 
 export class AddPlayerPage {
-  constructor(public viewCtrl: ViewController) {
-    
+  private addPlayerForm: FormGroup;
+
+  constructor(public viewCtrl: ViewController, private formBuilder: FormBuilder) {
+    this.addPlayerForm = this.formBuilder.group({
+      name: ['', Validators.required]
+    });
   }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+  
+  addPlayer(){
+    console.log("Added player " + this.addPlayerForm.value.name);
+    this.dismiss();
   }
 }
